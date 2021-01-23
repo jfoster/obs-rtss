26e051a741294765a04b89bcf17da5e323b73937
jfoster <jacobfoster@pm.me>
2021-01-23 02:33:14 +0000

    Add replay buffer saved event from obs 26.1

--- obs-rtss.cpp
+++ obs-rtss.cpp
@@ -12,18 +12,9 @@ OBS_MODULE_USE_DEFAULT_LOCALE("obs-rtss", "en-US")
 
 CRTSSSharedMemoryInterface *rtss;
 
-void ReleaseOSD()
-{
-	rtss->ReleaseOSD("obs-rtss");
-}
-
 void obs_osd_frontend_event_callback(enum obs_frontend_event event, void *private_data)
 {
-	ReleaseOSD();
 	switch (event) {
-		case OBS_FRONTEND_EVENT_FINISHED_LOADING:
-			break;
-
 		case OBS_FRONTEND_EVENT_STREAMING_STARTING:
 			rtss->UpdateOSD("obs-rtss-stream", "Streaming...");
 			break;
@@ -36,7 +27,6 @@ void obs_osd_frontend_event_callback(enum obs_frontend_event event, void *privat
 		case OBS_FRONTEND_EVENT_STREAMING_STOPPED:
 			rtss->ReleaseOSD("obs-rtss-stream");
 			break;
-
 		case OBS_FRONTEND_EVENT_RECORDING_STARTING:
 			rtss->UpdateOSD("obs-rtss-record", "Recording...");
 			break;
@@ -53,32 +43,6 @@ void obs_osd_frontend_event_callback(enum obs_frontend_event event, void *privat
 			break;
 		case OBS_FRONTEND_EVENT_RECORDING_UNPAUSED:
 			break;
-
-		case OBS_FRONTEND_EVENT_SCENE_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_SCENE_LIST_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_SCENE_COLLECTION_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_SCENE_COLLECTION_LIST_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_SCENE_COLLECTION_CLEANUP:
-			break;
-
-		case OBS_FRONTEND_EVENT_TRANSITION_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_TRANSITION_STOPPED:
-			break;
-		case OBS_FRONTEND_EVENT_TRANSITION_LIST_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_TRANSITION_DURATION_CHANGED:
-			break;
-
-		case OBS_FRONTEND_EVENT_PROFILE_CHANGED:
-			break;
-		case OBS_FRONTEND_EVENT_PROFILE_LIST_CHANGED:
-			break;
-
 		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTING:
 			break;
 		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STARTED:
@@ -87,15 +51,10 @@ void obs_osd_frontend_event_callback(enum obs_frontend_event event, void *privat
 			break;
 		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_STOPPED:
 			break;
-
-		case OBS_FRONTEND_EVENT_STUDIO_MODE_ENABLED:
-			break;
-		case OBS_FRONTEND_EVENT_STUDIO_MODE_DISABLED:
-			break;
-		case OBS_FRONTEND_EVENT_PREVIEW_SCENE_CHANGED:
-			break;
-
-		case OBS_FRONTEND_EVENT_EXIT:
+		case OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED:
+			rtss->UpdateOSD("obs-rtss-replay", "Clipped");
+			sleep(5);
+			rtss->ReleaseOSD("obs-rtss-replay");
 			break;
 	}
 }
@@ -103,12 +62,14 @@ void obs_osd_frontend_event_callback(enum obs_frontend_event event, void *privat
 bool obs_module_load(void)
 {
 	obs_frontend_add_event_callback(obs_osd_frontend_event_callback, 0);
-	rtss->UpdateOSD("obs-rtss", "obs-rtss");
 	return true;
 }
 
 void obs_module_unload() {
-	ReleaseOSD();
+	rtss->ReleaseOSD("obs-rtss");
+	rtss->ReleaseOSD("obs-rtss-stream");
+	rtss->ReleaseOSD("obs-rtss-record");
+	rtss->ReleaseOSD("obs-rtss-replay");
 }
 
 const char *obs_module_author(void) {
